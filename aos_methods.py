8a8fb075b8c3c4c9ec57cf70e0084cfe01793b2b
vladimirkapereula <vpereula@gmail.com>
2022-04-25 12:25:09 -0700

    update to aosaws project

--- aos_methods.py
+++ aos_methods.py
@@ -6,7 +6,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
-#   from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import Select
 import aos_locators as locators
 
 from selenium.webdriver.chrome.options import Options
